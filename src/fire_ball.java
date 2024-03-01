23f14e9aa516c0b9209ec5b5376cb4717c011cd2
schorio <schorioignace@gmail.com>
2024-03-01 19:10:02 +0300

    faire bouger le fusil

--- src/fire_ball.java
+++ src/fire_ball.java
@@ -13,13 +13,14 @@ public class fire_ball extends MIDlet {
 
     public void startApp() {
         display.setCurrent(gameCanvas);
+        new Thread(gameCanvas).start();
     }
 
     public void pauseApp() {}
 
     public void destroyApp(boolean unconditional) {}
 
-    class GameCanvas extends Canvas {
+    class GameCanvas extends Canvas implements Runnable {
         int playerX = 0;
         int playerY = getHeight() / 2;
         int dy = 0;
@@ -31,5 +32,29 @@ public class fire_ball extends MIDlet {
             g.fillRect(playerX, playerY, 7, 5);
         }
 
+        public void keyPressed(int keyCode) {
+            int action = getGameAction(keyCode);
+            if (action == UP) {
+                dy = -10;
+            } else if (action == DOWN) {
+                dy = 10;
+            }
+        }
+
+        public void keyReleased(int keyCode) {
+            dy = 0;
+        }
+
+        public void run() {
+            while (true) {
+                playerY += dy;
+                repaint();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
