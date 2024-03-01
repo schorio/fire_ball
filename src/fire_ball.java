7da5ac79a905d9c513ceb7865de507080c96b459
schorio <schorioignace@gmail.com>
2024-03-01 19:08:13 +0300

    creation du fusil pour tirer

--- src/fire_ball.java
+++ src/fire_ball.java
@@ -3,7 +3,33 @@ import javax.microedition.midlet.*;
 import java.util.*;
 
 public class fire_ball extends MIDlet {
+    private Display display;
+    private GameCanvas gameCanvas;
 
     public fire_ball() {
+        display = Display.getDisplay(this);
+        gameCanvas = new GameCanvas();
+    }
+
+    public void startApp() {
+        display.setCurrent(gameCanvas);
+    }
+
+    public void pauseApp() {}
+
+    public void destroyApp(boolean unconditional) {}
+
+    class GameCanvas extends Canvas {
+        int playerX = 0;
+        int playerY = getHeight() / 2;
+        int dy = 0;
+
+        public void paint(Graphics g) {
+            g.setColor(255, 255, 255);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(0, 0, 0);
+            g.fillRect(playerX, playerY, 7, 5);
+        }
+
     }
 }
